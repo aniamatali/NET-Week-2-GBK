6aac7b77f14bdf275c0f242e6e9f05b9e3ccd428
alvin niamatali <aniamatali@icloud.com>
2018-01-05 12:07:57 -0800

    changes and what not

--- Gummi/Models/Product.cs
+++ Gummi/Models/Product.cs
@@ -9,7 +9,7 @@ namespace Gummi.Models
 		[Key]
 		public int ProductId { get; set; }
 		public string Description { get; set; }
-        public int Price { get; set; }
+    public int Price { get; set; }
 		public int CategoryId { get; set; }
 		public virtual Category Category { get; set; }
 	}
